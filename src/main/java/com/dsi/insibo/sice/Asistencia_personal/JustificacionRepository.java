3a7600474c7e6473a6908b05e50a674a33aa432e
Manuel <aa21050@ues.edu.sv>
2024-11-02 22:42:37 -0600

    correciones y eliminacion de clases innecesarias

--- src/main/java/com/dsi/insibo/sice/Asistencia_personal/JustificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dsi.insibo.sice.Asistencia_personal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dsi.insibo.sice.entity.Justificacion;
-
-public interface JustificacionRepository extends JpaRepository <Justificacion, Integer>{
-       
-}
